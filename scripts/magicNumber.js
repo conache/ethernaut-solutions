af78205e42a33e4200b6df266b8238e9f7eab926
conache <C.conache@gmail.com>
2022-08-04 23:34:04 +0300

    MagicNum solution added

--- scripts/magicNumber.js
+++ scripts/magicNumber.js
@@ -3,18 +3,38 @@ const { web3, ethers } = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-  // const MagicNum = await ethers.getContractFactory("MagicNum");
-
-  // const magicNum = await MagicNum.attach("0x4E87ae14F2C79F4e20B361125a16Fbe3774BC15B");
-
-  const bytecode =
-    "0x6080604052348015600f57600080fd5b5060848061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063650500c114602d575b600080fd5b60336049565b6040805160ff9092168252519081900360200190f35b602a9056fea2646970667358221220e2137077d72c1c687ffe04184b7922d9d609409dde38c47e456e332304b406cc64736f6c634300060c0033";
+  const bytecode = "0x600a600c600039600a6000f3602a60805260206080f3";
+  // CONTRACT CREATION PART:
+  // [1] PUSH1 0x0a
+  // [3] PUSH1 0x0c
+  // [5] PUSH1 0x00
+  // [6] CODECOPY
+  // [8] PUSH1 0x0a
+  // [10] PUSH1 0x00
+  // [11] RETURN
+  // WRITING VALUE IN MEMORY AND RETURNING FROM THERE
+  // [13] PUSH1 0x2a
+  // [15] PUSH1 0x80
+  // [16] MSTORE
+  // [18] PUSH1 0x20
+  // [20] PUSH1 0x80
+  // [21] RETURN
   const returnVal = await web3.eth.sendTransaction({ from: deployer.address, data: bytecode }, function (err, res) {
     console.log("TX hash:", res);
   });
 
   const { contractAddress } = returnVal;
-  console.log("Contract address:", contractAddress);
+
+  console.log("Deployed address:", contractAddress);
+
+  await web3.eth
+    .sendTransaction({ from: deployer.address, to: contractAddress, data: web3.eth.abi.encodeFunctionSignature("whatIsTheMeaningOfLife()") })
+    .on("receipt", (receipt) => {
+      console.log("receipt:", receipt);
+    })
+    .on("error", (error) => {
+      console.log("error:", error);
+    });
 }
 
 main()
